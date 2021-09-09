eb8dca94c0c004ba327a8cf9813865fdb9d7ceef
Paul Riviera <paulr@nuolab.com>
2021-09-09 19:16:42 -0400

    add assert statement

--- QuizExample/QuizUnitTest/UnitTest1.cs
+++ QuizExample/QuizUnitTest/UnitTest1.cs
@@ -11,12 +11,23 @@ namespace QuizUnitTest
         public void CreateTrueFalseQuestion()
         {
             IQuestion q1 = QuestionFactory.NewQuestion(QuestionFactory.QuestionType.TrueOrFalse);
+            Assert.IsInstanceOfType(q1, typeof(TrueOrFalse));
 
-            if (q1.GetType() != typeof(TrueOrFalse))
-            {
-                throw new Exception();
-            }
-            
-        } 
+            // if (q1.GetType() != typeof(TrueOrFalse))
+            // {
+            //     throw new Exception();
+            // }
+        }
+
+        // [TestMethod]
+        // public void CreateTrueFalseQuestion()
+        // {
+        //     IQuestion q1 = QuestionFactory.NewQuestion(QuestionFactory.QuestionType.TrueOrFalse);
+
+        //     if (q1.GetType() != typeof(TrueOrFalse))
+        //     {
+        //         throw new Exception();
+        //     }
+        // } 
     }
 }
