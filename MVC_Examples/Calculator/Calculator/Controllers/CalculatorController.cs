62a0740143562d1c7e792f0fe9482b95a5763d61
Paul Riviera <PaulRiviera@outlook.com>
2021-11-02 00:27:18 +0000

    Add MVC Calculator example
    this example was from Mock Assessment 5

---
+++ MVC_Examples/Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,110 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: CalculatorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CalculatorController/Details/5
+        public ActionResult Result([Bind("num1", "num2", "operation")]Calculation calculation)
+        {
+
+            decimal result = 0;
+            switch (calculation.operation)
+            {
+                case "Plus": 
+                    result = calculation.num1 + calculation.num2; 
+                    break;
+                case "Minus":
+                    result = calculation.num1 - calculation.num2;
+                    break;
+                case "Multiply":
+                    result = calculation.num1 * calculation.num2;
+                    break;
+                case "Divide":
+                    result = calculation.num1 / calculation.num2;
+                    break;
+            }
+
+            ViewData["calculation"] = calculation;
+            ViewBag.Operation = calculation.operation;
+            ViewData["Result"] = result;
+
+            return View("Result");
+        }
+
+        // GET: CalculatorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CalculatorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalculatorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CalculatorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalculatorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CalculatorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_Examples/Calculator/Calculator/Models/Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class Calculation
+    {
+        public decimal num1 { get; set; }
+        public decimal num2 { get; set; }
+        public string operation { get; set; }
+    }
+}
