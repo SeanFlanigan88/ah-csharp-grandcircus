ed7b0a930f96b12657c8e0ae5f53d6efc784c7ee
Paul Riviera <PaulRiviera@outlook.com>
2021-11-11 02:14:25 +0000

    Examples from 2021.11.10

---
+++ WebAppToWebAPIExample/BasicWebAPI/BasicWebAPI/Models/CoffeeOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicWebAPI.Models
+{
+    public class CoffeeOrder
+    {
+        public string Drink { get; set; }
+        public decimal Price { get; set; }
+    }
+}
